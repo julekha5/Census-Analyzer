79d2a0116f541b8ffcffd30a578393107800ca2f
Julekha5 <mulani.julekha93@gmail.com>
2022-01-04 12:31:39 +0530

    Refactored : Test case written when csv file have incorrect header

--- src/test/java/com/bridgelabz/census/CensusAnalyzerTest.java
+++ src/test/java/com/bridgelabz/census/CensusAnalyzerTest.java
@@ -5,16 +5,15 @@ import org.junit.Test;
 
 public class CensusAnalyzerTest {
 
-    private static final String INDIA_CENSUS_CSV_PATH_DELIMITER = "E:\\eclipseProgram\\untitled\\Census-Analyzer\\src\\main//resources//IndiaStateCensusData.pdf";
+    private static final String INDIA_CENSUS_CSV_PATH_HEADER = "E:\\eclipseProgram\\untitled\\Census-Analyzer\\src\\main\\resources\\IndiaStateCensusDataHeader.csv";
 
-    //UseCase 1.4 Path is correct but delimiter incorrect
+    //UseCase 1.5 Path is correct but in csv file header incorrect
     @Test
-    public void givenIndianCensusCSVFileReturnsIncorrectDelimeter() {
+    public void givenIndianCensusCSVFileHaveIncorrectHeader() {
         CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
         try {
-              censusAnalyzer.loadIndiaCensusData(INDIA_CENSUS_CSV_PATH_DELIMETER);
-              Assert.assertTrue("Wrong delimiter", true);
-
+            censusAnalyzer.loadIndiaCensusData(INDIA_CENSUS_CSV_PATH_HEADER);
+            Assert.assertTrue("Wrong header in csv file", true);
         } catch (CensusAnalyzerException e) {
             e.printStackTrace();
         }
