a0d841373b36a6898ed4f0ade6163fa85911dca5
Julekha5 <mulani.julekha93@gmail.com>
2022-01-04 16:00:07 +0530

    Added : Happy test case for indian state code csv file have correct records

--- src/test/java/com/bridgelabz/census/CensusAnalyzerStateCodeTest.java
+++ src/test/java/com/bridgelabz/census/CensusAnalyzerStateCodeTest.java
@@ -5,18 +5,19 @@ import org.junit.Test;
 
 public class CensusAnalyzerStateCodeTest {
 
-    private static final String INDIA_CENSUS_CSV_PATH = "E:\\eclipseProgram\\untitled\\Census-Analyzer\\src\\main\\resources\\IndiaStateCode.csv";
+    private static final String INDIA_STATE_CSV_PATH = "E:\\eclipseProgram\\untitled\\Census-Analyzer\\src\\main\\resources\\IndiaStateCode.csv";
 
     //2.1 Happy Test case for correct record match
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
         CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
         try {
-            int numOfRecord = censusAnalyzer.loadIndiaStateCodeData(INDIA_CENSUS_CSV_PATH);
+            int numOfRecord = censusAnalyzer.loadIndiaStateCodeData(INDIA_STATE_CSV_PATH);
             Assert.assertEquals(37, numOfRecord);
         } catch (CensusAnalyzerException e) {
             e.printStackTrace();
         }
     }
+
 
 }
